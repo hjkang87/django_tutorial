dc71694ac8a5834ea0d042f7f80840d0511a92f3
Hyunjung Kang <kanghyunjung@gmail.com>
2018-05-22 22:23:49 +0900

    part5 - 2
    Signed-off-by: Hyunjung Kang <kanghyunjung@gmail.com>

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # from django.template import loader
 
 from .models import Choice, Question
@@ -32,12 +33,15 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 '''
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
